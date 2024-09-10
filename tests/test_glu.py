b27b439e25918b4fef788b0ff240cf9191e35683
Jarod Malestein <jdm@jarodcodes.dev>
2024-09-10 16:55:19 -0400

    Removing use of pytest_mock.mocker and using unittest.mock directly

--- tests/test_glu.py
+++ tests/test_glu.py
@@ -3,11 +3,23 @@ import glu
 import pytest
 import re
 import json
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 
 ARGV0 = "glu_test.py"
 TESTAPIKEY = "abc123xyzzy"
 COORDS1 = {"lat": "1.00", "lon": "-1.00"}
+
+@pytest.fixture(scope="function")
+def mock_get():
+    mg = patch('requests.get')
+    yield mg.start()
+    mg.stop()
+
+@pytest.fixture(scope="function")
+def mock_file():
+    mf = patch('builtins.open', mock_open(read_data=f'{TESTAPIKEY}\n'))
+    yield mf.start()
+    mf.stop()
 
 # Tests improperly formatted locations
 @patch("sys.argv", [ARGV0, "-k", f"key:{TESTAPIKEY}", "bad bad", "992291", "90210"])
@@ -25,9 +37,7 @@ def test_bad_location_inputs(capsys):
 @patch.dict("os.environ", {"GLUTESTOPENWEATHERMAPAPIKEY": TESTAPIKEY})
 @patch("glu.DEFAULT_ENV_VAR", "GLUTESTOPENWEATHERMAPAPIKEY")
 @patch("sys.argv", [ARGV0, "City, ZZ"])
-def test_default_env_unknown_location(mocker, capsys):
-    mock_get_response = mocker.MagicMock()
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
+def test_default_env_unknown_location(mock_get, capsys):
     glu.main()
     captured = capsys.readouterr()
     mock_get.assert_called_once()
@@ -38,36 +48,26 @@ def test_default_env_unknown_location(mocker, capsys):
 # Tests default key file location
 @patch.dict("os.environ", {"XXGLUTESTOPENWEATHERMAPAPIKEYXX": ""})
 @patch("glu.DEFAULT_ENV_VAR", "XXGLUTESTOPENWEATHEMAPRAPIKEYXX")
-def test_default_key_file_unknown_zip(mocker, capsys):
-    mock_get_response = mocker.MagicMock()
-    mock_get_response.json.return_value = {'code': 404}
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
-    mock_file = mocker.mock_open(read_data=f'{TESTAPIKEY}\n')
-    mocker.patch("builtins.open", mock_file)
-    mocker.patch("sys.argv", [ARGV0, "10203"])
+@patch("sys.argv", [ARGV0, "10203"])
+def test_default_key_file_unknown_zip(mock_get, mock_file, capsys):
+    mock_get.return_value.json.return_value = {'cod': 404}
     glu.main()
     captured = capsys.readouterr()
     mock_get.assert_called_once()
     assert mock_get.call_args.kwargs['params']['appid'] == TESTAPIKEY
     table_row = r'\s*10203\s*UNKNOWN\s*LOCATION'
-    assert re.search(table_row, captured.out)
+    assert len(re.findall(table_row, captured.out)) == 1
 
 # Tests --apikey key:123abc
 @patch("sys.argv", [ARGV0, "-k", f"key:{TESTAPIKEY}", "City, ZZ"])
-def test_apikey_key(mocker):
-    mock_get_response = mocker.MagicMock()
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
+def test_apikey_key(mock_get):
     glu.main()
     mock_get.assert_called_once()
     assert mock_get.call_args.kwargs['params']['appid'] == TESTAPIKEY
 
 # Tests --apikey file:./testkeyfile
-def test_apikey_file(mocker):
-    mock_get_response = mocker.MagicMock()
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
-    mock_file = mocker.mock_open(read_data=f'{TESTAPIKEY}\n')
-    mocker.patch("builtins.open", mock_file)
-    mocker.patch("sys.argv", [ARGV0, "--apikey", "file:testkeyfile", "City, ZZ"])
+@patch("sys.argv", [ARGV0, "--apikey", "file:testkeyfile", "City, ZZ"])
+def test_apikey_file(mock_get, mock_file):
     glu.main()
     mock_file.assert_called_once_with('testkeyfile')
     mock_get.assert_called_once()
@@ -76,9 +76,7 @@ def test_apikey_file(mocker):
 # Tests --apikey env:TEST_API_KEY_VAL
 @patch.dict('os.environ', {'GLUPYTESTKEYVALUE': TESTAPIKEY})
 @patch("sys.argv", [ARGV0, "--apikey", "env:GLUPYTESTKEYVALUE", "City, ZZ"])
-def test_apikey_env(mocker):
-    mock_get_response = mocker.MagicMock()
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
+def test_apikey_env(mock_get):
     glu.main()
     mock_get.assert_called_once()
     assert mock_get.call_args.kwargs['params']['appid'] == TESTAPIKEY
@@ -111,21 +109,22 @@ def test_apikey_bad_key_type(capsys):
     assert "Unable to parse --apikey parameter" in captured.err    
 
 @patch("sys.argv", [ARGV0, '-k', f'key:{TESTAPIKEY}', 'City One, ZZ', 'CityTwo,YY'])
-def test_location_parsing_direct(mocker, capsys):
-    mock_get_response = mocker.MagicMock()
-    mock_get_response.json.return_value = [COORDS1]
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
+def test_location_parsing_direct(mock_get, capsys):
     glu.main()
     captured = capsys.readouterr()
     assert mock_get.call_count == 2
     assert 'City One, ZZ, US' in captured.out
     assert 'CityTwo,YY, US' in captured.out
 
+@patch("sys.argv", [ARGV0, '-k', f'key:{TESTAPIKEY}', 'New City, ZZ'])
+def test_location_multiple_coords(mock_get, capsys):
+    mock_get.return_value.json.return_value = [COORDS1, COORDS1, COORDS1]
+    glu.main()
+    captured = capsys.readouterr()
+    assert len(re.findall(r'New City, ZZ, US', captured.out)) == 3
+
 @patch("sys.argv", [ARGV0, '-k', f'key:{TESTAPIKEY}', '11111', '22222'])
-def test_location_parsing_zip(mocker, capsys):
-    mock_get_response = mocker.MagicMock()
-    mock_get_response.json.return_value = COORDS1
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
+def test_location_parsing_zip(mock_get, capsys):
     glu.main()
     captured = capsys.readouterr()
     assert mock_get.call_count == 2
@@ -133,19 +132,14 @@ def test_location_parsing_zip(mocker, capsys):
     assert '22222' in captured.out
 
 @patch("sys.argv", [ARGV0, '-r', '-k', f'key:{TESTAPIKEY}', '   X Y Z  Z Y, X X, ZZ '])
-def test_location_raw(mocker):
-    mock_get_response = mocker.MagicMock()
-    mock_get_response.json.return_value = [COORDS1]
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
+def test_location_raw(mock_get):
     glu.main()
     mock_get.assert_called_once()
     assert mock_get.call_args.kwargs['params']['q'] == '   X Y Z  Z Y, X X, ZZ '
 
 @patch("sys.argv", [ARGV0, '-f', '-k', f'key:{TESTAPIKEY}', 'Xx, ZZ'])
-def test_fullresponse_output(mocker, capsys):
-    mock_get_response = mocker.MagicMock()
-    mock_get_response.json.return_value = [COORDS1]
-    mock_get = mocker.patch('requests.get', return_value=mock_get_response)
+def test_fullresponse_output(mock_get, capsys):
+    mock_get.return_value.json.return_value = [COORDS1]
     glu.main()
     captured = capsys.readouterr()
     mock_get.assert_called_once()
